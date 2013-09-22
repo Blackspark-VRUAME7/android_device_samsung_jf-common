ecdcd6db7dfe7d817c43d5a2ff3e28c902ff7ea4
bikedude880 <bikedude880@gmail.com>
2013-09-21 23:03:48 -0700

    kernel source location change

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,7 +26,7 @@
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/jf-common/include
 
 # Kernel
-TARGET_KERNEL_SOURCE         := kernel/samsung/jf
+TARGET_KERNEL_SOURCE         := kernel/samsung/jfltevzw
 BOARD_KERNEL_CMDLINE         := androidboot.hardware=qcom user_debug=31 zcache
 BOARD_KERNEL_BASE            := 0x80200000
 BOARD_MKBOOTIMG_ARGS         := --ramdisk_offset 0x02000000
